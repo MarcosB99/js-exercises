794e9b5adc0f9f39ee2b251127b6af0170b4fd04
Marcos Barrientos <74696345+MarcosB99@users.noreply.github.com>
2026-08-18 10:10:48 -0600

    Pushing final changes to script and styles files

--- js-project-odin-2/script.js
+++ js-project-odin-2/script.js
@@ -16,9 +16,22 @@ for (let i = 0; i < 16; i++) {
 }
 
 gridSize.addEventListener("click", () => {
-    let userInput = prompt("Please provide a grid size number: ");
-    if (userInput < 1 || userInput > 100 || userInput === !Number) {
+    let userInput = parseInt(prompt("Please provide a grid size number: "));
+    if (userInput < 1 || userInput > 100 || isNaN(userInput)) {
         alert("You need to chose a number between 1 and 100");
         return
+    }
+    subContainer.innerHTML = "";
+    for (let i = 0; i < userInput * userInput; i++) {
+        const square = document.createElement("div");
+        square.classList.add("new-grid-square");
+        let size = 100 / userInput;
+
+        square.style.width = size + "%";
+        square.style.height = size + "%";
+        subContainer.appendChild(square);
+        square.addEventListener("mouseover", () => {
+            square.classList.add("hovered");
+        });
     }
 });
