355bf9075d129b6fda4cf7094d66f8246166f1fc
Marcos Barrientos <74696345+MarcosB99@users.noreply.github.com>
2026-09-17 23:29:37 -0600

    Pushing another commit by EOD

--- cisco-js-exercises/script10.js
+++ cisco-js-exercises/script10.js
@@ -1,24 +1,33 @@
 const movies = [
-    {
-        title: "Last Day on Earth",
-        imdb: 5.5
-    }
+
 ];
-let isTrue;
+let isTrue = false;
 
 do {
     let movieName = prompt("Enter the name of the movie:");
-    let movieRating = Number(prompt("Enter the movie rating of IMDB:"));
+    let inputRating = prompt("Enter the movie rating of IMDB:");
 
-    if (!movieName || !movieRating) {
+    if (!movieName || !inputRating) {
         if (movies.length === 0) {
             console.log(`No movies were added to the array.`);
-            isTrue = true;
         } else {
-            console.log(`The list of movies added are:`);
-            for (let movie of movies) {
-                console.log(`${movie.title} (${movie.imdb})`);
+            for (let i = 0; i < movies.length; i++) {
+                if (movies[i].imdb < 7) {
+                    console.log(`The list of movies with a rating below 7 added are:`);
+                    for (let movie of movies) {
+                        console.log(`${movie.title} (${movie.imdb})`);
+                    }
+                }
             }
+        }
+        isTrue = true;
+    } else {
+        let movieRating = Number(inputRating);
+
+        if (Number.isNaN(movieRating)) {
+            console.log(`Please enter a valid number for the rating. Reload the screen.`);
+        } else {
+            movies.push({ title: movieName, imdb: movieRating });
         }
     }
 } while (!isTrue);
